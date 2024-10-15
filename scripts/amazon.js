0aeafc2a1fbe19d13e2f25710e8583fbaebdb59c
mathumithasrikanthan <mathumithasrikanthan@gmail.com>
2024-10-15 19:30:01 +0530

    Use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,29 +1,5 @@
-//save the data
-const products = [{
-  Image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name:  'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-   priceCents: 1090
-},{
-  Image: 'images/products/intermediate-composite-basketball.jpg',
-  name:  'Intermediate Size Basketball',
-  rating: {
-    stars: 4,
-    count: 127
-  },
-   priceCents: 2095
-},{
-  Image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name:  ' Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count:  56
-  },
-   priceCents: 799
-}];
+
+
 
 //combine this html together
 
