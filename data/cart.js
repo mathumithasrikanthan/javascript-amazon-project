9887329fa7987e7225a9070be704a49187df4861
mathumithasrikanthan <mathumithasrikanthan@gmail.com>
2024-10-16 23:38:31 +0530

    Move addToCard into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,21 @@
 export const cart = [];
+
+export function addToCart(productId) {
+    //when quantity += 1 and sometime same branchname item issue time while use id
+      let matchingItem;
+    
+      cart.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem;
+        }
+      });
+    
+        if (matchingItem) {
+          matchingItem.quantity += 1;
+      } else {
+          cart.push({
+            productId: productId,
+            quantity: 1
+        });
+      }
+    }
